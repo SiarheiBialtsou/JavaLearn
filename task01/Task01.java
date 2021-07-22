16805a5f370ea3be29b9db26816221ae54c4133e
SiarheiBialtsou <68424269+SiarheiBialtsou@users.noreply.github.com>
2021-07-22 08:27:33 +0300

    update tab with 4 symbol

--- task01/Task01.java
+++ task01/Task01.java
@@ -1,8 +1,6 @@
 /* №1. найти результат выражения z = ((a - 3) * b / 2) + c;
  */
-
 package by.java.training.task01;
-
 /**
  * @author Siarhei Bialtsou
  */
@@ -13,42 +11,40 @@ public class Task01 {
     public static void main(String[] args) {
         int a;
         int b;
-		int c;
-		double z;
+	int c;
+	double z;
+	/**
+	 * a = 5; b = 13; c = 2; hardcore data;
+	 */
+	Scanner sc = new Scanner(System.in);
 
-		/**
-		 * a = 5; b = 13; c = 2; hardcore data;
-		 */
+	System.out.println("Write basic data:");
 
-		Scanner sc = new Scanner(System.in);
-
-		System.out.println("Write basic data:");
-
-		System.out.print("Write a>");
-		while (!sc.hasNextInt()) {
-			sc.next();
-			System.out.print("WRITE a>");
-		}
-		a = sc.nextInt();
-
-		System.out.print("Write b>");
-		while (!sc.hasNextInt()) {
-			sc.next();
-			System.out.print("WRITE b>");
-		}
-		b = sc.nextInt();
-
-		System.out.print("Write c>");
-		while (!sc.hasNextInt()) {
-			sc.next();
-			System.out.print("WRITE c>");
-		}
-		c = sc.nextInt();
-		
-		sc.close();
-
-		z = ((a - 3) * (double) b / 2) + c;
-
-		System.out.print("Result equals > z = " + z);
+	System.out.print("Write a>");
+	while (!sc.hasNextInt()) {
+	    sc.next();
+	    System.out.print("WRITE a>");
 	}
+	a = sc.nextInt();
+
+	System.out.print("Write b>");
+	while (!sc.hasNextInt()) {
+	    sc.next();
+	    System.out.print("WRITE b>");
+	}
+	b = sc.nextInt();
+
+	System.out.print("Write c>");
+	while (!sc.hasNextInt()) {
+	    sc.next();
+	    System.out.print("WRITE c>");
+	}
+	c = sc.nextInt();			
+		
+	sc.close();				//закрытие сканера
+
+	z = ((a - 3) * (double) b / 2) + c; 	//вычисления результата
+
+	System.out.print("Result equals > z = " + z);
+    }
 }
