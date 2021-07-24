9da7f8b8c64ff2371213ebb3cb454775c1eced69
Siarhei Bialtsou <siarheibialtsou@gmail.com>
2021-07-24 10:52:27 +0300

    v 1

---
+++ task102/Task102.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package by.java.training.task102;
+
+/**
+ * @author zvezd
+ *
+ */
+public class Task102 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
